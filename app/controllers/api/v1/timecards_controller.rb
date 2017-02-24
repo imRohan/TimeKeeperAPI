041ebc3aa2c0012a606f5267fc69b490c34ed4ea
Rohan <rlikhite@gmail.com>
2017-02-24 16:00:20 -0500

    added error handling and show method

--- app/controllers/api/v1/timecards_controller.rb
+++ app/controllers/api/v1/timecards_controller.rb
@@ -1,6 +1,24 @@
 class Api::V1::TimecardsController < ApplicationController
+  before_action :get_timecard, only: [:show, :update, :destroy]
+
   def index
-    timecards = Timecard.all
-    render json: timecards
+    @timecards = Timecard.all
+    render json: @timecards
+  end
+
+  def show
+    render json: @timecard
+  end
+
+  private
+
+  def get_timecard
+    begin
+      @timecard = Timecard.find params[:id]
+    rescue ActiveRecord::RecordNotFound
+      timecard = Timecard.new
+      timecard.errors.add(:id, "No Timecard for this ID.")
+      render_error(timecard, 404) and return
+    end
   end
 end
