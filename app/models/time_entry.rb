bd964e38fb10ac627d2810eba7a94410e3d23938
Rohan <rlikhite@gmail.com>
2017-02-27 10:49:33 -0500

    check time entries, if odd add hours, else continue with save

--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -1,4 +1,18 @@
 class TimeEntry < ApplicationRecord
   validates :time, :timecard_id, presence: true
   belongs_to :timecard
+  before_create :check_if_timecard_complete
+
+  private
+  def check_if_timecard_complete
+    my_timecard = Timecard.find(self.timecard_id)
+    sibling_time_entries = my_timecard.time_entries
+    if sibling_time_entries.length%2 == 1
+      last_entry_time = sibling_time_entries.last.time.to_time
+      my_time = self.time.to_time
+      last_pair_time = ((my_time - last_entry_time)/60)/60
+      my_timecard.total_hours += last_pair_time
+      my_timecard.save
+    end
+  end
 end
