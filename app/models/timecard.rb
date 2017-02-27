ae72afc43d955779be898430c6665b1c62da814e
Rohan <rlikhite@gmail.com>
2017-02-27 09:39:37 -0500

    before create, downcase username param:

--- app/models/timecard.rb
+++ app/models/timecard.rb
@@ -3,4 +3,12 @@ class Timecard < ApplicationRecord
   validates :username, uniqueness: { scope: :occurrence }
 
   has_many  :time_entries, dependent: :destroy
+
+  before_create :normalize_params
+
+  private
+
+  def normalize_params
+    self.username.downcase!
+  end
 end
