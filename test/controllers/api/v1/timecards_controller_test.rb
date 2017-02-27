98fe4ede2f237d2314793eac4fef10d3442222bd
Rohan <rlikhite@gmail.com>
2017-02-27 11:40:35 -0500

    started writing tests

--- test/controllers/api/v1/timecards_controller_test.rb
+++ test/controllers/api/v1/timecards_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
+require 'json'
 
 class Api::V1::TimecardsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "Should get valid list of timecards" do
+    get "/api/v1/timecards"
+    assert_response :success
+    json_data = JSON.parse response.body
+    p json_data
+    assert_equal 5, json_data.length
+  end
 end
