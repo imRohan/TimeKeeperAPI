5a373d9a73dbda46fb2846069a5c7f78fe122094
Rohan <rlikhite@gmail.com>
2017-02-27 10:04:20 -0500

    fixed before_action, added show method, and error checking

--- app/controllers/api/v1/time_entries_controller.rb
+++ app/controllers/api/v1/time_entries_controller.rb
@@ -1,2 +1,24 @@
 class Api::V1::TimeEntriesController < ApplicationController
+  before_action :get_time_entry, only: [:show, :update, :destroy]
+
+  def index
+    @time_entries = TimeEntry.all
+    render json: @time_entries
+  end
+
+  def show
+    render json: @time_entry
+  end
+
+  private
+
+  def get_time_entry
+    begin 
+      @time_entry = TimeEntry.find params[:id]
+    rescue ActiveRecord::RecordNotFound
+      timeentry = TimeEntry.new
+      timeentry.errors.add(:id, "No Time Entry for this ID.")
+      render_error(timeentry, 422) and return
+    end
+  end
 end
